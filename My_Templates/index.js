7e757e3864ecb17a1ac7d063aefab6362228387b
chrislaiisme <chrislaiisme@gmail.com>
2024-05-14 08:15:07 +0800

    5/14 08:15

--- My_Templates/index.js
+++ My_Templates/index.js
@@ -28,7 +28,6 @@ function setContent(link, ele) {
 }
 
 async function init() {
-  await sleep(20);
   btns = document.querySelectorAll(".btn");
   codes = document.querySelectorAll(".code");
   strs = ["Default_Code", "LeetCode", "Generate", "Generate_Simple", "Match", "Special_Judge"];
@@ -37,12 +36,15 @@ async function init() {
   for(var i=0; i<codes.length; i++) {
     setEvent(btns[i], codes[i]);
     for(var j=0; j<headings[i].length; j++) {
+      await sleep(10);
       setContent(link + "Heading" + headings[i][j].toString() + ".txt", codes[i]);
-      await sleep(20);
+      await sleep(10);
     }
+    await sleep(10);
     codes[i].innerHTML += "\n";
+    await sleep(10);
     setContent(link + strs[i] + ".txt", codes[i]);
-    await sleep(20);
+    await sleep(10);
   }
 }
 
